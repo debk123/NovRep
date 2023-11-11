a8a4954e1b068e7e9f6e63e93450318d008dd23c
raj <raj@gmail.com>
2023-11-12 02:12:46 +0530

    f

---
+++ simple/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simple
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("welcome to jenkins");
+        }
+        public static string mess()
+        {
+            return "jenkins";
+        }
+    }
+}
